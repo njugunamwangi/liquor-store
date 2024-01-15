2177e0b7f9e879403e5faaab1c9e7ea483a41134
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-15 13:12:16 +0300

    Hot reload

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
             refresh: [
                 ...refreshPaths,
                 'app/Livewire/**',
+                'app/Filament/**',
             ],
         }),
     ],
