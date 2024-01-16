62ff0f2bc4aa762be930c804b44c102c9bf180c8
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-16 11:20:04 +0300

    Install aspect ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import aspectRatio from '@tailwindcss/aspect-ratio';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -21,5 +22,5 @@ export default {
         },
     },
 
-    plugins: [forms, typography, '@tailwindcss/aspect-ratio'],
+    plugins: [forms, typography, aspectRatio],
 };
