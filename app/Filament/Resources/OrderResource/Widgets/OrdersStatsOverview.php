7ad375802ab890740bfc25515a1b944613e17c07
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-17 17:37:47 +0300

    Refactor order stats

--- app/Filament/Resources/OrderResource/Widgets/OrdersStatsOverview.php
+++ app/Filament/Resources/OrderResource/Widgets/OrdersStatsOverview.php
@@ -6,6 +6,7 @@ use App\Enums\PaymentStatus;
 use App\Models\Order;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
 
 class OrdersStatsOverview extends BaseWidget
 {
@@ -15,12 +16,12 @@ class OrdersStatsOverview extends BaseWidget
             Stat::make('Total Orders', Order::count())
                 ->icon('heroicon-o-shopping-bag')
                 ->description('Total number of orders'),
-            Stat::make('Total Amount', Order::sum('total_price'))
+            Stat::make('Total Amount', Number::currency(Order::sum('total_price'), 'KES'))
                 ->icon('heroicon-o-credit-card')
                 ->description('Total amount payable'),
-            Stat::make('Total Revenue', Order::query()
+            Stat::make('Total Revenue', Number::currency(Order::query()
                 ->where('payment_status', PaymentStatus::Paid)
-                ->sum('total_price'))
+                ->sum('total_price'), 'KES'))
                 ->icon('heroicon-o-banknotes')
                 ->description('Total amount paid'),
         ];
