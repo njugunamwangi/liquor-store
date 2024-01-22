8a3d2769b7b91f527819d041e52965dc6d28a8ce
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-22 22:13:03 +0300

    Category factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $category = $this->faker->name();
+        $slug = Str::of($category)->slug();
+
+        return [
+            'category' => $category,
+            'slug' => $slug,
+            'image_id' => $this->faker->unique()->numberBetween(1, 5)
+        ];
+    }
+}
