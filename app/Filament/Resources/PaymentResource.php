c80df416aca581afe358a6d2a8a8b66341ce7998
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-27 02:29:07 +0300

    Payment resource

---
+++ app/Filament/Resources/PaymentResource.php
@@ -0,0 +1,184 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PaymentResource\Pages;
+use App\Filament\Resources\PaymentResource\RelationManagers;
+use App\Models\Payment;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PaymentResource extends Resource
+{
+    protected static ?string $model = Payment::class;
+
+    protected static ?string $navigationGroup = 'Shop';
+    protected static ?int $navigationSort = 7;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('order_id')
+                    ->relationship('order', 'id')
+                    ->required(),
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('message')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('paymentId')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('domain')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('gateway_response')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('reference')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('currency')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('ip_address')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('amount')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('channel')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('order.order_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('message')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('paymentId')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('status')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('domain')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('gateway_response')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('reference')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('currency')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('ip_address')
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('amount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('channel')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                // Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make('Payment Information')
+                    ->icon('heroicon-o-credit-card')
+                    ->columns(2)
+                    ->schema([
+                        TextEntry::make('message'),
+                        TextEntry::make('gateway_response'),
+                        TextEntry::make('paymentId')
+                            ->label('PayStack Payment ID'),
+                        TextEntry::make('reference')
+                            ->label('PayStack Reference ID'),
+                        TextEntry::make('ip_address')
+                            ->label('IP Address'),
+                        TextEntry::make('amount')
+                            ->getStateUsing(function($record) {
+                                return $record->currency . number_format($record->amount, 2);
+                            })
+                    ]),
+                Section::make('Order Information')
+                    ->icon('heroicon-o-shopping-bag')
+                    ->columns(2)
+                    ->schema([
+                        TextEntry::make('order.order_id')
+                            ->label('Order Reference'),
+                        TextEntry::make('order.order_status')
+                            ->label('Order Status')
+                            ->badge()
+                            ->color(function($state) {
+                                return $state->getColor();
+                            }),
+                        TextEntry::make('user.name')
+                            ->label('Name'),
+                        TextEntry::make('user.email')
+                            ->label('Email'),
+                    ])
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPayments::route('/'),
+            // 'create' => Pages\CreatePayment::route('/create'),
+            'view' => Pages\ViewPayment::route('/{record}'),
+            // 'edit' => Pages\EditPayment::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/PaymentResource/Pages/ViewPayment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\PaymentResource\Pages;
+
+use App\Filament\Resources\PaymentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewPayment extends ViewRecord
+{
+    protected static string $resource = PaymentResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            // Actions\EditAction::make(),
+        ];
+    }
+}
