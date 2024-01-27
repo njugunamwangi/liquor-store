03f78ad6a65dad4cb2cebddc4fd94bcd224f876a
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-27 18:55:54 +0300

    Customer chart

---
+++ app/Filament/Widgets/CustomersOrderChart.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\User;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class CustomersOrderChart extends ChartWidget
+{
+    protected static ?string $heading = 'Customer Registration';
+
+    public ?string $filter = '3months';
+
+    protected function getFilters(): ?array {
+        return [
+            'week' => 'Last Week',
+            'month' => 'Last Month',
+            '3months' => 'Last 3 Months',
+        ];
+    }
+
+    protected function getData(): array
+    {
+        $filter = $this->filter;
+
+        match ($filter) {
+            'week' => $data = Trend::model(User::class)
+                ->between(
+                    start: now()->subWeek(),
+                    end: now(),
+                )
+                ->perDay()
+                ->count(),
+            'month' => $data = Trend::model(User::class)
+                ->between(
+                    start: now()->subMonth(),
+                    end: now(),
+                )
+                ->perDay()
+                ->count(),
+            '3months' => $data = Trend::model(User::class)
+                ->between(
+                    start: now()->subMonths(3),
+                    end: now(),
+                )
+                ->perMonth()
+                ->count(),
+        };
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Customer Registration',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
