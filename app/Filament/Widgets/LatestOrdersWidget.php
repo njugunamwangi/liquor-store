9a9d4e218f17d2e2b8f2597a079038b5e213fbc3
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-27 19:13:53 +0300

    Latest order

---
+++ app/Filament/Widgets/LatestOrdersWidget.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Filament\Resources\OrderResource;
+use App\Models\Order;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class LatestOrdersWidget extends BaseWidget
+{
+    protected int | string | array $columnSpan = 'full';
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(OrderResource::getEloquentQuery())
+            ->defaultSort('created_at', 'desc')
+            ->columns([
+                Tables\Columns\TextColumn::make('tracking_no')
+                    ->label('Tracking Number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Customer')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_price')
+                    ->prefix('Kes ')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('order_status')
+                    ->badge()
+                    ->color(function ($state) {
+                        return $state->getColor();
+                    })
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('payment_status')
+                    ->badge()
+                    ->color(function ($state) {
+                        return $state->getColor();
+                    })
+                    ->searchable(),
+            ])
+            ->actions([
+                Tables\Actions\Action::make('open')
+                    ->url(fn (Order $record): string => OrderResource::getUrl('view', ['record' => $record])),
+            ]);
+    }
+}
