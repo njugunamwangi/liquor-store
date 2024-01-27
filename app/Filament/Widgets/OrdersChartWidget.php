cd2ab4843eb24615e084bed88c545d67b3a34851
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-27 18:56:07 +0300

    Order chart  widget

---
+++ app/Filament/Widgets/OrdersChartWidget.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class OrdersChartWidget extends ChartWidget
+{
+    protected static ?string $heading = 'Order History';
+
+    public ?string $filter = 'month';
+
+    protected function getFilters(): ?array {
+        return [
+            'week' => 'Last Week',
+            'month' => 'Last Month',
+            '3months' => 'Last 3 Months',
+        ];
+    }
+
+    protected function getData(): array
+    {
+        $filter = $this->filter;
+
+        match ($filter) {
+            'week' => $data = Trend::model(Order::class)
+                ->between(
+                    start: now()->subWeek(),
+                    end: now(),
+                )
+                ->perDay()
+                ->count(),
+            'month' => $data = Trend::model(Order::class)
+                ->between(
+                    start: now()->subMonth(),
+                    end: now(),
+                )
+                ->perDay()
+                ->count(),
+            '3months' => $data = Trend::model(Order::class)
+                ->between(
+                    start: now()->subMonths(3),
+                    end: now(),
+                )
+                ->perMonth()
+                ->count(),
+        };
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Orders',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}
