c085267d1dc5360c107225c85309e14136d2b494
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-28 19:39:00 +0300

    Database notifications

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -63,6 +63,8 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
+            ->databaseNotifications()
+            ->databaseNotificationsPolling('30s')
             ->resources([
                 config('filament-logger.activity_resource')
             ])
