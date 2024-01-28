e87c5a8367e89ebce23ceacbc3e126403292b184
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-28 19:39:39 +0300

    Order Observer

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Order;
+use Filament\Notifications\Notification;
+
+class OrderObserver
+{
+    /**
+     * Handle the Order "created" event.
+     */
+    public function created(Order $order): void
+    {
+        Notification::make()
+            ->title('New order '. $order->order_id)
+            ->sendToDatabase($order->user);
+    }
+
+    /**
+     * Handle the Order "updated" event.
+     */
+    public function updated(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "deleted" event.
+     */
+    public function deleted(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "restored" event.
+     */
+    public function restored(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "force deleted" event.
+     */
+    public function forceDeleted(Order $order): void
+    {
+        //
+    }
+}
