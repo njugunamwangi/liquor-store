99ffa686dd3ba5749a38779478273519f599d02b
halifaxmundia <halifaxmundia@gmail.com>
2024-02-03 21:16:10 +0300

    Add savour & type to products

---
+++ database/migrations/2024_02_03_173549_add_savour_and_type_to_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Savour;
+use App\Models\Type;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->foreignIdFor(Savour::class)->nullable()->constrained();
+            $table->foreignIdFor(Type::class)->nullable()->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('savour_id');
+            $table->dropColumn('type_id');
+        });
+    }
+};
