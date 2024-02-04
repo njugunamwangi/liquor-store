3aaa35af4d1132cef5606f7404fc5487cadada0c
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-04 23:46:43 +0300

    User resource table refactor

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -15,6 +15,7 @@ use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -84,14 +85,19 @@ class UserResource extends Resource
                 Tables\Columns\TextColumn::make('email_verified_at')
                     ->dateTime()
                     ->sortable(),
+                TextColumn::make('roles.name')
+                    ->searchable()
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('two_factor_confirmed_at')
                     ->dateTime()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('current_team_id')
                     ->numeric()
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('profile_photo_path')
-                    ->searchable(),
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
