61cc14bd5508f7e0f7fd7687c4ea00ee878cb277
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-05 13:48:22 +0300

    Implement logout

--- resources/views/components/layout/navbar.blade.php
+++ resources/views/components/layout/navbar.blade.php
@@ -337,20 +337,51 @@
                 <div class="flex flex-1 items-center justify-end">
                     <!-- Search -->
                     <a href="#" class="ml-6 hidden p-2 text-gray-400 hover:text-gray-500 lg:block">
-                    <span class="sr-only">Search</span>
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                    </svg>
+                        <span class="sr-only">Search</span>
+                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
                     </a>
+
+                    <!-- Cart -->
+                    <div class="ml-4 flow-root lg:ml-6">
+                        <a href="{{ route('cart') }}" wire:navigate class="group -m-2 flex items-center p-2">
+                            <svg class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                            </svg>
+                            <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                                <livewire:cart.count />
+                            </span>
+                            <span class="sr-only">items in cart, view bag</span>
+                        </a>
+                    </div>
 
                     <!-- Account -->
                     @if(\Illuminate\Support\Facades\Auth::check())
-                        <a href="{{ route('orders') }}" wire:navigate class="p-2 text-gray-400 hover:text-gray-500 lg:ml-4">
+                        <a href="{{ route('orders') }}" wire:navigate class="m-4 text-gray-400 hover:text-gray-500 lg:ml-4">
                             <span class="sr-only">Account</span>
                             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                             </svg>
                         </a>
+
+                        <form method="POST" action="{{ route('logout') }}" x-data>
+
+                            @csrf
+
+                            <a href="{{ route('logout') }}" @click.prevent="$root.submit();" class="p-2 text-gray-400 hover:text-gray-500 lg:ml-4">
+                                <svg class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 256 256" xml:space="preserve">
+                                    <defs>
+                                    </defs>
+                                    <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;" transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)" >
+                                        <path d="M 69.88 53.615 c -0.198 0 -0.399 -0.059 -0.574 -0.182 c -0.451 -0.317 -0.561 -0.941 -0.243 -1.394 l 10.775 -15.335 L 69.063 21.369 c -0.317 -0.452 -0.208 -1.075 0.243 -1.393 c 0.452 -0.317 1.077 -0.208 1.394 0.244 l 11.18 15.911 c 0.242 0.345 0.242 0.805 0 1.149 L 70.699 53.19 C 70.505 53.467 70.194 53.615 69.88 53.615 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                        <path d="M 57.988 73.41 H 40.677 c -0.552 0 -1 -0.447 -1 -1 s 0.448 -1 1 -1 h 16.312 V 45 c 0 -0.552 0.447 -1 1 -1 s 1 0.448 1 1 v 27.41 C 58.988 72.963 58.541 73.41 57.988 73.41 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                        <path d="M 57.988 0 H 8.939 c -0.004 0 -0.007 0.002 -0.01 0.002 C 8.812 0.003 8.698 0.029 8.587 0.071 c -0.026 0.01 -0.051 0.021 -0.076 0.034 c -0.03 0.014 -0.062 0.023 -0.09 0.04 C 8.358 0.182 8.304 0.23 8.252 0.279 C 8.245 0.286 8.235 0.291 8.228 0.298 C 8.148 0.38 8.088 0.475 8.04 0.578 c -0.012 0.025 -0.021 0.05 -0.031 0.076 C 7.968 0.764 7.939 0.879 7.939 1 v 71.41 c 0 0.121 0.029 0.236 0.07 0.345 c 0.011 0.03 0.024 0.058 0.038 0.087 c 0.042 0.088 0.096 0.168 0.162 0.24 c 0.013 0.014 0.021 0.03 0.034 0.042 c 0.069 0.067 0.146 0.126 0.233 0.171 l 31.738 16.59 C 40.359 89.962 40.518 90 40.677 90 c 0.18 0 0.359 -0.049 0.518 -0.145 c 0.299 -0.182 0.482 -0.506 0.482 -0.855 V 17.59 c 0 -0.372 -0.207 -0.714 -0.537 -0.886 L 13.011 2 h 43.978 v 25.698 c 0 0.552 0.447 1 1 1 s 1 -0.448 1 -1 V 1 C 58.988 0.448 58.541 0 57.988 0 z M 39.677 87.349 L 9.939 71.805 V 2.651 l 29.738 15.545 V 87.349 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                        <path d="M 81.061 37.705 H 47.596 c -0.553 0 -1 -0.448 -1 -1 s 0.447 -1 1 -1 h 33.465 c 0.553 0 1 0.448 1 1 S 81.613 37.705 81.061 37.705 z" style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;" transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
+                                    </g>
+                                </svg>
+                            </a>
+                        </form>
                     @else
                         <a href="{{ route('login') }}" wire:navigate class="p-2 text-gray-400 hover:text-gray-500 lg:ml-4">
                             <span class="sr-only">Account</span>
@@ -360,18 +391,7 @@
                         </a>
                     @endif
 
-                    <!-- Cart -->
-                    <div class="ml-4 flow-root lg:ml-6">
-                    <a href="{{ route('cart') }}" wire:navigate class="group -m-2 flex items-center p-2">
-                        <svg class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                        </svg>
-                        <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                            <livewire:cart.count />
-                        </span>
-                        <span class="sr-only">items in cart, view bag</span>
-                    </a>
-                    </div>
+
                 </div>
                 </div>
             </div>
