c16699cc74ee0b45eb36279e8b56018d86141d8a
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-05 11:20:21 +0300

    Liquor amount

--- resources/views/components/product-item.blade.php
+++ resources/views/components/product-item.blade.php
@@ -8,7 +8,7 @@
             </div>
             <div class="mt-4">
                 <h3 class="text-sm font-medium text-gray-900">{{ $product->product }}</h3>
-                <p class="mt-1 text-sm text-gray-500">{{ $product->brand->brand }} | {{ $product->flavor->flavor }}</p>
+                <p class="mt-1 text-sm text-gray-500">{{ $product->brand->brand }} | {{ $product->flavor->flavor }} | {{ $product->amount->amount }} </p>
                 <p class="mt-1 text-sm font-medium text-gray-900">Kes {{ number_format($product->retail_price, 2) }} </p>
             </div>
         </div>
