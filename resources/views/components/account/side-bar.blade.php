31283fb107b5e2e95acb70bb52412ce0472622f3
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-07 16:26:40 +0300

    Mobile menu

--- resources/views/components/account/side-bar.blade.php
+++ resources/views/components/account/side-bar.blade.php
@@ -1,5 +1,6 @@
+<div x-data="{ mobile: false }">
 <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
-<div class="relative z-50 lg:hidden" role="dialog" aria-modal="true">
+    <div class="relative z-50 lg:hidden" x-show="mobile" x-cloak @click.away="mobile = false" role="dialog" aria-modal="true">
         <!--
         Off-canvas menu backdrop, show/hide based on off-canvas menu state.
 
@@ -35,7 +36,7 @@
                 To: "opacity-0"
             -->
             <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
-                <button type="button" class="-m-2.5 p-2.5">
+                <button type="button" @click="mobile = !mobile"  class="-m-2.5 p-2.5">
                     <span class="sr-only">Close sidebar</span>
                     <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -240,7 +241,7 @@
     </div>
 
     <div class="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden">
-        <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
+        <button type="button" @click="mobile = !mobile"  class="-m-2.5 p-2.5 text-gray-700 lg:hidden">
             <span class="sr-only">Open sidebar</span>
             <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -252,3 +253,4 @@
         <img class="h-8 w-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
         </a>
     </div>
+</div>
