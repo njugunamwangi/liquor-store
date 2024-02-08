16f3c80e0592ec46bed059ab4317368eac555840
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-08 14:16:46 +0300

    Custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout >
+    <main class="mx-auto flex w-full max-w-7xl flex-auto flex-col justify-center px-6 py-24 sm:py-64 lg:px-8">
+        <p class="text-base font-semibold leading-8 text-indigo-600">404</p>
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+        <p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+        <div class="mt-10">
+        <a href="/" wire:navigate class="text-sm font-semibold leading-7 text-indigo-600"><span aria-hidden="true">&larr;</span> Back to home</a>
+        </div>
+    </main>
+</x-app-layout>
