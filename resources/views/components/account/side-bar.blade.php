10ba04988b419b05f47679b568f3f80f6ea04912
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-08 10:57:26 +0300

    Livewire navigate

--- resources/views/components/account/side-bar.blade.php
+++ resources/views/components/account/side-bar.blade.php
@@ -46,7 +46,7 @@
 
             <!-- Sidebar component, swap this element with another sidebar if you like -->
             <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
-                <a href="/">
+                <a href="/" wire:navigate>
                     <div class="flex h-16 shrink-0 items-center">
                         <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
                     </div>
@@ -158,7 +158,7 @@
     <div class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         <!-- Sidebar component, swap this element with another sidebar if you like -->
         <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
-        <a href="/" class="flex h-16 shrink-0 items-center">
+        <a href="/" class="flex h-16 shrink-0 items-center" wire:navigate>
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
         </a>
         <nav class="flex flex-1 flex-col">
