b3a0ccc69317d8d39ce199ab4a6774e2570f1b4f
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-13 14:53:44 +0300

    Seed 10 test products

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\Amount;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Flavor;
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
-class ProductSeeder  extends Seeder
+class ProductSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        Product::factory(10)->create();
     }
 }
