ed7585a7b99cdc415e1309ef35963edc269eb3df
njugunamwangi <njugunamwangi.n@gmail.com>
2024-02-22 17:36:00 +0300

    Set brand

--- resources/views/livewire/brands.blade.php
+++ resources/views/livewire/brands.blade.php
@@ -113,6 +113,16 @@
                                         @foreach($hoveredBrand->products as $product)
                                             <x-category-item :product="$product" />
                                         @endforeach
+                                    @else
+                                        @foreach($brands as $brand)
+                                            <div wire:click="setHoveredBrand({{ $brand->id }})" class="relative flex h-80 w-56 flex-col overflow-hidden rounded-lg p-6 hover:opacity-75 xl:w-auto">
+                                                <span aria-hidden="true" class="absolute inset-0">
+                                                <img src="{{ empty($brand->featured_image_id) ? "https://placehold.jp/30/200x300.png?text=image" : url('/storage/'. $brand->featuredImage->path) }}" alt="{{ $brand->brand }}" class="h-full w-full object-cover object-center">
+                                                </span>
+                                                <span aria-hidden="true" class="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50"></span>
+                                                <span class="relative mt-auto text-center text-xl font-bold text-white">{{ $brand->brand }}</span>
+                                            </div>
+                                        @endforeach
                                     @endif
                                 </div>
                             </div>
