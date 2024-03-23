a8fed54b9c440ac7065fcafd86e266e9fe2835c4
njugunamwangi <njugunamwangi.n@gmail.com>
2024-03-23 16:30:22 +0300

    Refactor layout & title

--- app/Livewire/Brands.php
+++ app/Livewire/Brands.php
@@ -3,20 +3,23 @@
 namespace App\Livewire;
 
 use App\Models\Brand;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Brands extends Component
 {
     public $hoveredBrand;
 
+    #[Layout('components.layouts.app')]
+    #[Title('Brands')]
     public function render()
     {
         $brands = Brand::query()
             ->has('products')
             ->get();
 
-        return view('livewire.brands', compact('brands'))
-            ->layout('components.layouts.app', ['title' => 'Brands']);
+        return view('livewire.brands', compact('brands'));
     }
 
     public function setHoveredBrand($brand) {
--- app/Livewire/Flavors.php
+++ app/Livewire/Flavors.php
@@ -3,20 +3,23 @@
 namespace App\Livewire;
 
 use App\Models\Flavor;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Flavors extends Component
 {
     public $hoveredFlavor;
 
+    #[Layout('components.layouts.app')]
+    #[Title('Flavors')]
     public function render()
     {
         $flavors = Flavor::query()
             ->has('products')
             ->get();
 
-        return view('livewire.flavors', compact('flavors'))
-            ->layout('components.layouts.app', ['title' => 'Flavors']);
+        return view('livewire.flavors', compact('flavors'));
     }
 
     public function setHoveredFlavor($flavor) {
