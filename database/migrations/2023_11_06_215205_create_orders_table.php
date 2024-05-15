c0c1c36e42c17741601ba797c442272bf334e572
njugunamwangi <njugunamwangi.n@gmail.com>
2024-05-15 23:06:13 +0300

    Migrations refactor

--- database/migrations/2023_11_06_215205_create_orders_table.php
+++ database/migrations/2023_11_06_215205_create_orders_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Enums\OrderStatus;
+use App\Enums\PaymentStatus;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,10 +16,15 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
             $table->decimal('total_price', 10, 2);
             $table->string('payment_method', 45)->nullable();
-            $table->string('payment_status', 45);
+            $table->enum('payment_status', PaymentStatus::values())->default(PaymentStatus::DEFAULT);
+            $table->string('order_id');
+            $table->enum('order_status', OrderStatus::values())->default(OrderStatus::DEFAULT);
+            $table->string('tracking_no', 2000);
+            $table->decimal('shipping', 10, 2);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
